d31a5680b2efa95cf63db41c47b83cd24f28ff5f
ssgier <sandro.sgier@gmail.com>
2022-05-22 10:59:14 +0200

    Initial commit

---
+++ src/main/java/io/github/ssgier/laketools/loader/Main.java
@@ -0,0 +1,11 @@
+package io.github.ssgier.laketools.loader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private final static Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        logger.info("Application starting");
+    }
+}
